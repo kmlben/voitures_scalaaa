5e1aa6c5a8ecafb2b9c18e53995e9b04b041d6f7
A <aoussedj.camelia@gmail.com>
2021-10-15 08:23:18 +0200

    okayyy

---
+++ Vehicules.scala
@@ -0,0 +1,6 @@
+Vehicules.scala
+
+sealed abstract class Vehicule[A]{}
+case class Car[T] extends Vehicule[T]{}
+case class Motorbike[T] extends Vehicule[T]{}
+case class Boat[T] extends Vehicule[T]{}
